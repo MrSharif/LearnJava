2c6a709b9bc61dc0d2ed0ea627bee467a929c979
Fashi Sharif <fashi.sharif@gmail.com>
2017-05-29 21:27:42 -0500

    Created simple calculator

---
+++ Tutorial3/src/calculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class calculator {
+	public static void main(String args[]){
+		
+		Scanner kbd = new Scanner(System.in);
+		
+		double fnum, snum, answer;
+		
+		System.out.println("Enter first Number: ");
+		
+		fnum = kbd.nextDouble();
+		
+		System.out.println("Enter second Number: ");
+		
+		snum = kbd.nextDouble();
+		
+		answer = fnum + snum;
+		
+		System.out.println("Total: " + answer);
+	}
+}
