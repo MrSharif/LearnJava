d7844e979dbe1c9e8b497b5b1325e99c0af72d40
Fashi Sharif <fashi.sharif@gmail.com>
2017-05-30 21:34:06 -0500

    Learned how to increment

---
+++ Tutorial3/src/increment.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class increment {
+	public static void main(String args[]){
+		
+	Scanner kbd = new Scanner(System.in);
+	
+	int tuna = 5;
+	int bass = 8;
+	
+	//tuna = tuna + 5;
+	tuna += 5;
+	
+	//System.out.println(++tuna);  take 5 adds 1 then print >> out put 6
+	//System.out.println(tuna++);  prints 5 then adds 1 >> out put 5
+	System.out.println(tuna);
+	
+	}
+}
