d62e21b666471bb2d4fc269ead619a995ee26ded
Fashi Sharif <fashi.sharif@gmail.com>
2017-06-01 21:29:57 -0500

    build a calculator, learned about if statemnt and logic operators

---
+++ Tutorial3/src/ifStatement.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ifStatement {
+	public static void main(String args[]){
+		
+		Scanner kbd = new Scanner(System.in);
+		
+		int test = 1;
+		
+		while( test != 6){
+			
+			test++;
+			if(test == 6){
+				System.out.println("Great");
+			}
+			else{
+				System.out.println("Not Great");
+			}
+			
+			//test++;
+		}
+		
+	}
+}
---
+++ Tutorial3/src/logicOperators.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class logicOperators {
+	public static void main(String args[]){
+		
+			int boy,girl;
+			
+			boy = 18;
+		 	girl = 68;
+			
+			if (boy > 10 || girl < 60){
+				System.out.println("you can enter");
+			}
+			else {
+				System.out.println("You are too young");
+			}
+	}
+}
