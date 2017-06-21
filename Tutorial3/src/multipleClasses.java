8a3d482f1f551a9e70af7781fad42d40aad0b9c2
Fashi Sharif <fashi.sharif@gmail.com>
2017-06-20 21:19:17 -0500

    learned about classes and how to use them

---
+++ Tutorial3/src/multipleClasses.java
@@ -0,0 +1,10 @@
+import java.applet.*;
+
+public class multipleClasses {
+	public static void main(String args[]){
+		
+		usingClasses classesObject = new usingClasses();
+		
+		classesObject.simpleMessage();
+	}
+}
---
+++ Tutorial3/src/usingClasses.java
@@ -0,0 +1,7 @@
+
+public class usingClasses {
+	public void simpleMessage(){
+		
+		System.out.println("This is another Class");
+	}
+}
